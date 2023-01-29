dccc05c149dd3be6b3f006a6a9570b18df03a310
rpispitler <6354761+nathgrin@users.noreply.github.com>
2023-01-30 00:00:56 +0100

    failed use camera

--- coords.py
+++ coords.py
@@ -6,7 +6,14 @@ from leds import cycle_individual_lights
 
 def main():
 	
-	print("Hello")
+	import picamera2 as picam
+	
+	camera = picam.Picamera2()
+	
+	camera.start_and_capture_file("tst.jpg")
+	
+	# ~ cycle_individual_lights()
+	
 	
 	
 if __name__ == "__main__":
--- leds.py
+++ leds.py
@@ -1,9 +1,14 @@
+from utils import get_strip
+import time
+
+
 def cycle_individual_lights(color:tuple[int]=(255,255,255),
-        dt: float=1., loop: bool=True,
+        dt: float=1., loop: int=True,
         off_color:tuple[int]=(0,0,0)):
 
     with get_strip() as strip:
         strip.fill( off_color )
+        strip.show()
         while True:
 
             for i in range(len(strip)):
