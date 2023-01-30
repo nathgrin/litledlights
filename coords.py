eca2dc785a5dbcfe9814a481fc7f6a2db1301557
rpispitler <6354761+nathgrin@users.noreply.github.com>
2023-01-31 00:25:10 +0100

    webcam struggles

--- coords.py
+++ coords.py
@@ -1,20 +1,129 @@
+
+import cv2
+import time
+
 from utils import get_strip,clear
 
-from leds import cycle_individual_lights
+from leds import blink_binary
 
+def show_wait_and_capture(strip,cam,dt:float=0.001,grayscale: bool=True, fname: str="tmp.png"):
+	
+	strip.show()
+	
+	time.sleep(dt)
+	
+	ret,frame  = cam.read()
+	if grayscale:
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		
+		
+	cv2.imwrite( fname, frame )
+		
+	return ret,frame
+	
+def tst():
 
+	cam = cv2.VideoCapture(0)
+	
+	cv2.namedWindow("test")
+	
+	img_counter = 0
+	
+	while True:
+	    ret, frame = cam.read()
+	    if not ret:
+	        print("failed to grab frame")
+	        break
+	    cv2.imshow("test", frame)
+	
+	    k = cv2.waitKey(1)
+	    if k%256 == 27:
+	        # ESC pressed
+	        print("Escape hit, closing...")
+	        break
+	    elif k%256 == 32:
+	        # SPACE pressed
+	        img_name = "opencv_frame_{}.png".format(img_counter)
+	        cv2.imwrite(img_name, frame)
+	        print("{} written!".format(img_name))
+	        img_counter += 1
+	
+	cam.release()
+	cv2.destroyAllWindows()
+
+def tst2():
+	loc = "_tmp/"
+	vcap = cv2.VideoCapture(0)
+	
+	
+	
+	with get_strip() as strip:
+		
+		
+		print(" - Frame off")
+		ret, frame = vcap.read()
+		cv2.imwrite(loc+"im_off.png", frame)
+		
+		
+		print(" - Frame on")
+		strip.fill( (255,255,255) )
+		strip.show()
+		for t in range(1050):
+			ret, frame = vcap.read()
+			if ret and t % 1000 == 0:
+				cv2.imwrite(loc+"im_on.jpg".format(t), frame)
+			time.sleep(0.001)
+			
+			
+		vcap.release()
+		
+	
 
 def main():
+	# ~ tst2()
+	# ~ exit()
+	cam = cv2.VideoCapture(0)
+	# Settings
+	loc = "_tmp/"
+	# ~ cam.set(38,1)
+	# Set auto exposure to false
+	# ~ cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75)
+	# ~ exposure = 0.5
+	# ~ cam.set(cv2.CAP_PROP_EXPOSURE, exposure)
 	
-	import picamera2 as picam
+	cv2.namedWindow("tst")
 	
-	camera = picam.Picamera2()
-	
-	camera.start_and_capture_file("tst.jpg")
-	
-	# ~ cycle_individual_lights()
-	
-	
+	with get_strip() as strip:
+		
+		try:
+			print(" - Off image")
+			# ~ strip.fill( (255,255,255) )
+			strip.fill( (0,0,0) )
+			
+			ret,im_off = show_wait_and_capture(strip,cam,fname=loc+"im_off.png")
+			
+			print(" - On  image")
+			strip.fill( (255,255,255) )
+			# ~ strip.fill( (0,0,0) )
+			strip.show()
+			# For some stupid reason we have to do this wait kind of thing
+			# Well its stupid and it takes long
+			print("   wait for it ...")
+			for t in range(1000):
+				ret,frame = cam.read()
+				time.sleep(0.001)
+			
+			ret,im_on = show_wait_and_capture(strip,cam,fname=loc+"im_on.png")
+			
+			subtraction = cv2.subtract( im_on,im_off )
+			cv2.imwrite( loc+"im_subtract.png", subtraction )
+			
+			cv2.imshow( "tst", subtraction )
+			cv2.waitKey(0)
+			
+		finally: 
+			cam.release()
+			cv2.destroyAllWindows()
 	
 if __name__ == "__main__":
 	main()
--- leds.py
+++ leds.py
@@ -2,21 +2,69 @@ from utils import get_strip
 import time
 
 
-def cycle_individual_lights(color:tuple[int]=(255,255,255),
-        dt: float=1., loop: int=True,
+def cycle_sequential(strip=None,
+        color:tuple[int]=(255,255,255),
+        dt: float=1., loop: bool=False,
         off_color:tuple[int]=(0,0,0)):
-
-    with get_strip() as strip:
+    
+    strip = get_strip() if strip is None else strip
+    
+    with strip:
         strip.fill( off_color )
         strip.show()
         while True:
 
-            for i in range(len(strip)):
-                strip[i-1] = off_color
-                strip[i]   = color
+            for l in range(len(strip)):
+                strip[l-1] = off_color
+                strip[l]   = color
                 strip.show()
                 time.sleep(dt)
             if not loop:
                 break
 
+
+def blink_binary(inlist: list[int],nbits: int=None,
+        strip=None,
+        color:tuple[int]=(255,255,255),
+        dt: float=1., loop: int=False,
+        off_color:tuple[int]=(0,0,0)):
+    
+    strip = get_strip() if strip is None else strip
+    
+    
+    bins = [format(n, 'b') for n in inlist]
+    lens = [ len(n) for n in bins ]
+    nbits = max(lens) if nbits is None else max(max(lens),nbits) # Watch out, theres no warning here!
+    bins = [ x.zfill(nbits) for x in bins ]
+    # ~ print(bins)
+    # ~ print(lens)
+    
+    with strip:
+        while True:
+            strip.fill( off_color )
+            strip.show()
+            
+            for i in range(nbits):
+                for l in range(len(strip)):
+                    strip[l] = color if int(bins[l][i]) else off_color
+                
+                strip.show()
+                time.sleep(dt)
+                
+            if not loop:
+                break
+        
+        
+        
+        
+
+    
+def main():
+    strip = get_strip()
+    inlist = range(len(strip))
+    blink_binary(inlist,strip=strip)
+    
+
+if __name__ == "__main__":
+    main()
     
