0119933f82b2827eb69e36d4471e08fc522e5f11
nathgrin <6354761+nathgrin@users.noreply.github.com>
2024-07-09 00:26:26 +0200

    merge

--- litledlights/calibrate/findlights.py
+++ litledlights/calibrate/findlights.py
@@ -40,10 +40,6 @@ def find_light_neuralnet(img,
     
     results = nnmodel.predict(img,verbose=False,classes=[0])[0] # only detect litledlights
     
-<<<<<<< HEAD
-    if len(results) > 0:
-        x,y = float(results.boxes.xywh[0][0]),float(results.boxes.xywh[0][1])
-=======
     print("Neuralnet!")
     
     results = nnmodel(img)
@@ -52,7 +48,6 @@ def find_light_neuralnet(img,
     
     if res.boxes:
         pass
->>>>>>> 7286ea9281c72c7baa9fbce20efd7d0ed3dcdb4f
     else:
         y,x = np.nan,np.nan
         
