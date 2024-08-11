95383228ffa1ba787bb8a3a1c9703378c7b5f70c
rpispitler <6354761+nathgrin@users.noreply.github.com>
2024-08-11 22:39:13 +0200

    add activatelll and tmp

---
+++ activatelll.sh
@@ -0,0 +1,5 @@
+source ~/Desktop/env/bin/activate
+PYTHON3_VENV_PATH=$(which python3)
+alias pylll="sudo -E env PATH=$PATH python3"
+alias lll="pylll ~/Desktop/litledlights/litledlights/lll.py"
+cdlll
